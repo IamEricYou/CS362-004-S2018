c9c7ad3ae93ccb5594602bcdf4b2f0cea9d3b63a
Eric You <bapsajura@naver.com>
2018-06-01 14:42:36 -0700

    little bit fixed

--- FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -9,7 +9,6 @@ import junit.framework.TestCase;
 
 
 public class UrlValidatorTest extends TestCase {
-	UrlValidator urlValid = new UrlValidator();
 	
    public UrlValidatorTest(String testName) {
       super(testName);
@@ -19,7 +18,7 @@ public class UrlValidatorTest extends TestCase {
    
    public void testManualTest()
    {
-	   //System.out.println(urlValid.isValid("https://www.google.com"));
+	   System.out.println(urlValid.isValid("http://www.google.com"));
 	   //You can use this function to implement your manual testing	   
 	   //assertFalse(urlValid.isValid("http://www.google.com"));
 	  
@@ -44,43 +43,54 @@ public class UrlValidatorTest extends TestCase {
 	   /*
 	    *  ResultPair.java (it had a bug, so I've fixed it)
 	    * */
-	   
-	   //http://formvalidation.io/validators/uri/
-	   
-	   String[] testunit = {
-			   "http://www.google.com",
-			   "http://www.amazon.com",
-			   "http://foo.com/blah_blah",
-			   "http:///a",
-			   "http://userid:password@example.com/",
-			   "http://-error-.invalid/",
-			   "http://255.255.255.255",
-			   "http://userid@example.com:8080",
-			   "http://##/##",
-			   "http://google",
-			   "http",
-			   "https://www.google.com",
-			   "httt://www.google.com",
-			   "ftp://w22ww.1234.1234"
-	   };
-	   
-	   ResultPair[] testpair = {
-			   new ResultPair("http://www.google.com", true),
-			   new ResultPair("http://www.amazon.com", true),
-			   new ResultPair("http://foo.com/blah_blah", true),
-			   new ResultPair("http:///a", false),
-			   new ResultPair("http://userid:password@example.com/", true),
-			   new ResultPair("http://-error-.invalid/", false),
-			   new ResultPair("http://255.255.255.255", true),
-			   new ResultPair("http://userid@example.com:8080", true),
-			   new ResultPair("http://##/##", false),
-			   new ResultPair("http://google", false),
-			   new ResultPair("http", false),
-			   new ResultPair("https://www.google.com", true),
-			   new ResultPair("httt://www.google.com", false), 
-			   new ResultPair("ftp://w22ww.1234.1234", false) 
-	   };
-	   
+		UrlValidator urlValid = new UrlValidator(null,null,UrlValidator.ALLOW_ALL_SCHEMES);
+	
+		ResultPair[] testpair = {
+			//Scheme test
+			new ResultPair("http://www.google.com", true),
+			// new ResultPair("ftp://www.google.com", true),
+			// new ResultPair("https://www.google.com", true),
+			// new ResultPair("htt://www.google.com", false),
+			// new ResultPair("http/www.google.com", false),
+			// new ResultPair("http:/?www.google.com", false),
+			// new ResultPair("http:///www.google.com", false),
+			
+			//Authority test
+			new ResultPair("http://www.amazon.com", true),
+			new ResultPair("http://255.255.255.255", true),
+			new ResultPair("http://www.oregonstate.edu", true),
+			new ResultPair("http://-error-.invalid/", false),
+			new ResultPair("http://...", false),
+			new ResultPair("http://w22ww.1234.1234", false),
+			new ResultPair("http://##/##", false),
+			new ResultPair("http://15.15.15.15", false),
+		
+			
+			//Port test
+			new ResultPair("http://userid@example.com:8080", true),
+			new ResultPair("http://www.google.com:8000", true),
+			new ResultPair("http://www.google.com:80", true),
+			new ResultPair("http://www.google.com:-10", false),
+			new ResultPair("http://www.google.com:66666", false),
+			new ResultPair("http://www.google.com:8238a", false),
+
+			//Path test
+			new ResultPair("http://www.google.com/index", true),
+			new ResultPair("http://www.google.com/index/", true),
+			new ResultPair("http://www.google.com/temp/temp", true),
+			new ResultPair("http://www.google.com/t88", true),
+			new ResultPair("http://www.google.com/../", false),
+			new ResultPair("http://www.google.com/../..", false),
+			new ResultPair("http://www.google.com/..//index", false),
+
+			//Query test
+			new ResultPair("http://www.google.com?action=view", true),
+			new ResultPair("http://www.google.com?action=edit=temp", true),
+			new ResultPair("http://www.google.com?set=true&temp=val", true),
+			new ResultPair("http://www.google.com??!?$", false),
+			new ResultPair("http://www.google.com!#", false)
+		};
+		
 	   /*System.out.println("Checking in result pair");
 	   for(int i=0; i < testpair.length; i++) {
 		   System.out.println("Check the URL: " + testpair[i].item + " -> " + testpair[i].valid );
@@ -91,18 +101,16 @@ public class UrlValidatorTest extends TestCase {
 		   System.out.println("Check the URL: " + testunit[i]+ " -> " + urlValid.isValid(testunit[i]));
 	   }*/
 	   
-	   int good_count = 0;
 	   int bad_count = 0;
 	   
 	   System.out.println("Check the result");
-	   for(int i = 0; i < testunit.length; i++) {
-		   if(testpair[i].valid != urlValid.isValid(testunit[i])) {
+	   for(int i = 0; i < testpair.length; i++) {
+		   if(testpair[i].valid != urlValid.isValid(testpair[i].item)) {
 			   bad_count++;
 			   System.out.println("This case is failed: " + testpair[i].item );
 		   }
-		   good_count++;
 	   }
-	   System.out.println("The result is: " + bad_count + " failed out of " + good_count);
+	   System.out.println("The result is: " + bad_count + " failed out of " + testpair.length);
    }
    
 }
